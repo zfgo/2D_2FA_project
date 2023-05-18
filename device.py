7aab470c5aa389d7c21b91dbd1c402bb5eb92370
zfgo <89813559+zfgo@users.noreply.github.com>
2023-05-17 23:00:07 -0700

    add file headers and function descriptions

--- device.py
+++ device.py
@@ -1,5 +1,14 @@
-# created 2023-04-27 by Zane Globus-O'Harra
-# 
+"""
+2D2FA device
+
+Get the network information (host, port), as well as the user
+information and the one-time identifier created by the server. Generate
+a PIN based on the identifier, the current time, and using the user's
+secret key as the hash key. Send this PIN to the server for
+verification. 
+
+created 2023-04-27 by Zane Globus-O'Harra
+"""
 
 import time
 import hashlib, hmac
@@ -22,39 +31,70 @@ TIME_SLICE = 30 # a time slice is 30s as defined in the 2d-2fa paper
 HOST = "127.0.0.1"
 PORT = 65432
 
-# functions to get user and key info
-# fixed returns for this version
-# production version could select user via input or QR code
-# and key from stored key(s)
+
 def get_user():
+    """
+    Dummy function for getting the user. Currently only the default
+    'test_user' is returned in this version. 
+
+    production version could select user via input and the key from
+    stored key(s)
+    """
     return "test_user"
 
-# gets the key for this device (single user)
-# production version would probably read this from disk
+
 def get_key():
+    """
+    Get the secret key associated with a user. This is read in from the
+    disk and provided to the pin generation algorithm.
+    """
     return "test_key"
 
 
-# function to return an identifier
-# for testing, using a fixed value
-# for production, prompt for user entry or use command line
 def get_identifier():
-    # return 123456
+    """
+    function for getting an identifier as input from the user, already
+    created and provided by the server.
+    """
     val = int(input("Enter identifier: "))
     return val
 
 
 def set_host():
+    """
+    Get the host IP address. If no host is specified (empty string is 
+    entered), then the default host is returned (127.0.0.1)
+    """
     h = input("Enter host address: ")
+
+    if h == "":
+        # return default local host if no host specified
+        return "127.0.0.1"
+
     return h
 
 
 def set_port():
-    p = int(input("Enter port number: "))
-    return p
+    """
+    Set the port. The user must enter the same port that the server is
+    running on for a connection to be established. If no port is
+    specified (empty string is entered), then a default port of 4444 is
+    returned. 
+    """
+    entered = input("Enter port number: ")
+    if entered == "":
+        # return default port of 4444 if no host specified
+        return 4444
+
+    return int(entered)
 
 
 def generate_pin(identifier):
+    """
+    Generate a pin using the entered identifier, the time, and the
+    user's secret key. This is done using the SHA256 hash algorithm.
+    This generated pin is sent to the server to be verified.
+    """
     time_s = int(time.time()) # get the time since epoch in seconds
     time_slice = time_s // TIME_SLICE # get the time, divide to get current slice
     
@@ -74,14 +114,17 @@ def generate_pin(identifier):
 
 
 def main():
+    # set the host and port
     host = set_host()
-    # print("Host set to: ", host)
     port = set_port()
-    id = get_identifier()
+
+    # get the user and the identifier
     user = get_user()
+    id = get_identifier()
+
+    # generate the pin
     pin = generate_pin(id)
-    # print("Generated PIN: ", pin)
-    # deviceutils.send_message(HOST, PORT, user, pin)
+    # send the pin to the server
     deviceutils.send_message(host, port, user, pin)
 
 
--- deviceutils.py
+++ deviceutils.py
@@ -1,8 +1,13 @@
-# 2D2FA Device Utilities
-# created 2023-05-05 by Doug Ure
+"""
+2D2FA Device Utilities
 
-# TCP connection and messaging code modified from:
-# https://realpython.com/python-sockets/
+TODO
+
+created 2023-05-05 by Doug Ure
+
+TCP connection and messaging code modified from:
+https://realpython.com/python-sockets/
+"""
 
 import sys
 import socket
@@ -20,6 +25,21 @@ DEBUG = False
 
 
 def create_request(user, pin):
+    """
+    Create a request, which is a dict in the following format:
+    ```
+    {
+        "type": "text/json",
+        "encoding": "utf-8",
+        "content": {
+            "user": user,
+            "pin": pin,
+        },
+    }
+    ```
+    It has a default type and encoding, but takes in the user's name and
+    the generated pin as arguments.
+    """
     return dict(
         type="text/json",
         encoding="utf-8",
@@ -28,27 +48,46 @@ def create_request(user, pin):
 
 
 def start_connection(host, port, request):
-    addr = (host, port)
+    """
+    Connect to the server to send a message. Get the correct address
+    from the host and port from the user, and connect to a remote socket
+    at the address. Create a Message object to send over the connection
+    and register it.
+    """
+    addr = (host, port) # get the address
+
     if DEBUG:
         print(f"Starting connection to {addr}")
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(False)
-    sock.connect_ex(addr)
+    sock.connect_ex(addr) # connect to the remote socket at the address
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    message = Message(sel, sock, addr, request)
+    message = Message(sel, sock, addr, request) # create the message
+
     sel.register(sock, events, data=message)
 
 
 def send_message(host, port, user, pin):
+    """
+    Create a request that will be sent over the connection, start the
+    connection, and send the message over the connection. Close the
+    scoket and unregister the message when complete.
+    """
     request = create_request(user, pin)
     start_connection(host, port, request)
+
     try:
         while True:
             events = sel.select(timeout=1)
+
+            # for each message, attempt to send it over the network
             for key, mask in events:
                 message = key.data
                 try:
+                    # send the message over the network
                     message.process_events(mask)
+
                 except Exception:
                     print(
                         f"Main: Error: Exception for {message.addr}:\n"
@@ -58,14 +97,45 @@ def send_message(host, port, user, pin):
             # Check for a socket being monitored to continue.
             if not sel.get_map():
                 break
+    
     except KeyboardInterrupt:
         print("Caught keyboard interrupt, exiting")
+
     finally:
+        # close the selector
         sel.close()
 
 
 class Message:
+    """
+    A class to represent a message and network connection, capable of
+    sending the message over the network to a server, and closing the
+    socket over which the message was sent. 
+    """
     def __init__(self, selector, sock, addr, request):
+        """
+        The Message class initializer initializes the following
+        attributes:
+
+        - selector: A selector object for "high-level and efficient I/O
+        - multiplexing." This determines if a message is available for
+          reading or writing. 
+        - sock: The socket that provides the connection to the server.
+        - addr: The address of the server to which the socket is
+          connected.
+        - request: The 'request' data structure created by
+          `create_request()`.
+        - _recv_buffer: Buffer into which data is read from the socket
+          connection. 
+        - _send_buffer: Buffer into which data is written before it is
+          sent over the connection.
+        - _request_queued: Boolean indicating whether a request has been
+          queued for sending over the network.
+        - _jsonheader_len: The length of a `jsonheader`.
+        - jsonheader: The header of a message that is to be sent over
+          the network.
+        - response: The response from the server.
+        """
         self.selector = selector
         self.sock = sock
         self.addr = addr
@@ -75,10 +145,12 @@ class Message:
         self._request_queued = False
         self._jsonheader_len = None
         self.jsonheader = None
-        self.response = None
+        self.esponse = None
 
     def _set_selector_events_mask(self, mode):
-        """Set selector to listen for events: mode is 'r', 'w', or 'rw'."""
+        """
+        Set selector to listen for events: mode is 'r', 'w', or 'rw'.
+        """
         if mode == "r":
             events = selectors.EVENT_READ
         elif mode == "w":
@@ -90,6 +162,10 @@ class Message:
         self.selector.modify(self.sock, events, data=self)
 
     def _read(self):
+        """
+        Get data from the socket connection, put it into the
+        `_recv_buffer`. 
+        """
         try:
             # Should be ready to read
             data = self.sock.recv(4096)
@@ -103,6 +179,10 @@ class Message:
                 raise RuntimeError("Peer closed.")
 
     def _write(self):
+        """
+        If there is data in the `_send_buffer`, send it over the socket
+        connection. 
+        """
         if self._send_buffer:
             if DEBUG:
                 print(f"Sending {self._send_buffer!r} to {self.addr}")
@@ -116,9 +196,17 @@ class Message:
                 self._send_buffer = self._send_buffer[sent:]
 
     def _json_encode(self, obj, encoding):
+        """
+        Helper function to encode a JSON object using a specified
+        encoding. Return the encoded object.
+        """
         return json.dumps(obj, ensure_ascii=False).encode(encoding)
 
     def _json_decode(self, json_bytes, encoding):
+        """
+        Helper function to decode a JSON object using a specified
+        encoding. Return the decoded object.
+        """
         tiow = io.TextIOWrapper(
             io.BytesIO(json_bytes), encoding=encoding, newline=""
         )
@@ -129,6 +217,11 @@ class Message:
     def _create_message(
         self, *, content_bytes, content_type, content_encoding
     ):
+        """
+        Create a message to send over the network by packing the message
+        header and the message into a struct. Return the created
+        message. 
+        """
         jsonheader = {
             "byteorder": sys.byteorder,
             "content-type": content_type,
@@ -141,21 +234,35 @@ class Message:
         return message
 
     def _process_response_json_content(self):
+        """
+        Helper function to process JSON header content.
+        """
         content = self.response
         result = content.get("result")
         print(f"Got result: {result}")
 
     def _process_response_binary_content(self):
+        """
+        Helper function to process binary content.
+        """
         content = self.response
         print(f"Got response: {content!r}")
 
     def process_events(self, mask):
+        """
+        Based on the mask set in the selector, either write to or read
+        from the network.
+        """
         if mask & selectors.EVENT_READ:
             self.read()
         if mask & selectors.EVENT_WRITE:
             self.write()
 
     def read(self):
+        """
+        Call the `_read()` helper function, then process the header
+        received from the server and process the response. 
+        """
         self._read()
 
         if self._jsonheader_len is None:
@@ -170,6 +277,10 @@ class Message:
                 self.process_response()
 
     def write(self):
+        """
+        Write to the network. Queue a request, and the call the helper
+        function to send the message over the socket connection.
+        """
         if not self._request_queued:
             self.queue_request()
 
@@ -181,8 +292,12 @@ class Message:
                 self._set_selector_events_mask("r")
 
     def close(self):
+        """
+        Close the socket connection to an address.
+        """
         if DEBUG:
             print(f"Closing connection to {self.addr}")
+
         try:
             self.selector.unregister(self.sock)
         except Exception as e:
@@ -200,6 +315,12 @@ class Message:
             self.sock = None
 
     def queue_request(self):
+        """
+        Queue a request for sending. Create the message header and pack
+        it with the message by calling the `_create_message()` helper.
+        Then, pack the message into the `_send_buffer` and set the
+        `_request_queued` indicator.
+        """
         content = self.request["content"]
         content_type = self.request["type"]
         content_encoding = self.request["encoding"]
@@ -228,6 +349,11 @@ class Message:
             self._recv_buffer = self._recv_buffer[hdrlen:]
 
     def process_jsonheader(self):
+        """
+        Process a JSON message header. Decode the json, and set crop the
+        `_recv_buffer` so that the message header is excluded from the
+        actual message contents.
+        """
         hdrlen = self._jsonheader_len
         if len(self._recv_buffer) >= hdrlen:
             self.jsonheader = self._json_decode(
@@ -244,6 +370,11 @@ class Message:
                     raise ValueError(f"Missing required header '{reqhdr}'.")
 
     def process_response(self):
+        """
+        Process a response based on the header type of the received
+        message. Once the message has been processed, close the socket
+        connection. 
+        """
         content_len = self.jsonheader["content-length"]
         if not len(self._recv_buffer) >= content_len:
             return
